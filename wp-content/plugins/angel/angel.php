4ea3d7fbe335162ad497254e3250ebb4934dee2c
Derek S <derek@storkey.co.uk>
2020-03-22 10:04:36 +0000

    Added base plugin and theme

---
+++ wp-content/plugins/angel/angel.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: Midhurst Angels
+Plugin URI:
+Description: Functions required for Midhurst Angels
+Author: Derek Storkey
+Version: 0.1
+Author URI:
+*/
+
+class MidhurstAngels {
+
+  public function __construct(){
+
+  }
+
+}
+
+$angels = new MidhurstAngels();
+
+ ?>
